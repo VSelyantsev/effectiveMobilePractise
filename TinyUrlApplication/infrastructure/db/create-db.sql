b50d1f466ef7fb9114f65b7c3c75103a2066aa4e
VSelyantsev <vselyantsev@mail.ru>
2024-10-28 18:14:05 +0300

    added dockerfile for tiny url app

--- TinyUrlApplication/infrastructure/db/create-db.sql
+++ TinyUrlApplication/infrastructure/db/create-db.sql
@@ -1,2 +1,15 @@
-create USER postgres;
-create DATABASE tiny_url;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'tiny_url') THEN
+        CREATE DATABASE tiny_url;
+    END IF;
+END $$;
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_user WHERE pg_user.usename = 'postgres') THEN
+        CREATE USER postgres;
+    END IF;
+END $$;
+
+GRANT ALL PRIVILEGES ON DATABASE tiny_url TO postgres;
