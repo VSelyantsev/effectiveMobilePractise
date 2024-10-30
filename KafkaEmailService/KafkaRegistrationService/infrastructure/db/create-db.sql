b995022a0e45744b1a1f28c335436cedd780b7cd
VSelyantsev <vselyantsev@mail.ru>
2024-10-30 16:50:09 +0300

    added dockerfile and compose for services with kafka

--- KafkaEmailService/KafkaRegistrationService/infrastructure/db/create-db.sql
+++ KafkaEmailService/KafkaRegistrationService/infrastructure/db/create-db.sql
@@ -1,2 +1,15 @@
-CREATE USER postgres;
-CREATE DATABASE auth_service_db;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'auth_service_db') THEN
+        CREATE DATABASE auth_service_db;
+    END IF;
+END $$;
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_user WHERE pg_user.usename = 'postgres') THEN
+        CREATE USER postgres;
+    END IF;
+END $$;
+
+GRANT ALL PRIVILEGES ON DATABASE auth_service_db TO postgres;
